f23683da7b425e0c06814130d7ae0a3ae97ad544
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/PopupMenuCustomization/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿Namespace PopupMenuCustomization
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            Me.panel1 = New System.Windows.Forms.Panel()
+            Me.label1 = New System.Windows.Forms.Label()
+            Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panel1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.DataStorage = Me.schedulerStorage1
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 32)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.Size = New System.Drawing.Size(701, 325)
+            Me.schedulerControl1.Start = New Date(2010, 10, 8, 0, 0, 0, 0)
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            ' 
+            ' panel1
+            ' 
+            Me.panel1.Controls.Add(Me.label1)
+            Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panel1.Location = New System.Drawing.Point(0, 0)
+            Me.panel1.Name = "panel1"
+            Me.panel1.Size = New System.Drawing.Size(701, 32)
+            Me.panel1.TabIndex = 1
+            ' 
+            ' label1
+            ' 
+            Me.label1.AutoSize = True
+            Me.label1.Location = New System.Drawing.Point(200, 9)
+            Me.label1.Name = "label1"
+            Me.label1.Size = New System.Drawing.Size(285, 13)
+            Me.label1.TabIndex = 0
+            Me.label1.Text = "Right-click in the Scheduler area for a custom popup menu"
+            Me.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+            ' 
+            ' defaultLookAndFeel1
+            ' 
+            Me.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Colorful"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(701, 357)
+            Me.Controls.Add(Me.schedulerControl1)
+            Me.Controls.Add(Me.panel1)
+            Me.Name = "Form1"
+            Me.Text = "Custom Popup Menu Example"
+            CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panel1.ResumeLayout(False)
+            Me.panel1.PerformLayout()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private panel1 As System.Windows.Forms.Panel
+        Private label1 As System.Windows.Forms.Label
+        Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    End Class
+End Namespace
+
