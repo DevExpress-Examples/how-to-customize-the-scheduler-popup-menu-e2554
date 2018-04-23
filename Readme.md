a85a9647db3d9b2a4510b4fdfc2bbbc59ae57a1a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize the Scheduler popup menu
+
+
+<p>This example illustrates how to use the <strong>PopupMenuShowing</strong> event of the SchedulerControl to add, modify, remove or hide menu items of the Scheduler popup menu.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-customize-the-scheduler-popup-menu-e2554/13.1.4+/media/f796b593-631a-11e7-80c0-00155d624807.png"></p>
+
+<br/>
+
+
---
+++ VB/PopupMenuCustomization/Helper.vb
@@ -0,0 +1,26 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.XtraScheduler
+
+Namespace PopupMenuCustomization
+	Friend Class Helper
+		Public Shared Users() As String = { "Peter Dolan", "Ryan Fischer", "Andrew Miller", "Tom Hamlett", "Jerry Campbell", "Carl Lucas", "Mark Hamilton", "Steve Lee" }
+		Public Shared Sub FillResources(ByVal storage As SchedulerStorage, ByVal count As Integer)
+			Dim resources As ResourceCollection = storage.Resources.Items
+			storage.BeginUpdate()
+			Try
+				Dim cnt As Integer = Math.Min(count, Users.Length)
+				For i As Integer = 1 To cnt
+					Dim resource As Resource = storage.CreateResource(i)
+					resource.Caption = Users(i - 1)
+					resources.Add(resource)
+				Next i
+			Finally
+				storage.EndUpdate()
+			End Try
+		End Sub
+
+	End Class
+End Namespace
